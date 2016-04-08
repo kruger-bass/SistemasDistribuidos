d2882eec66a5efd0948898ac18d4d2d08a06d477
Marmota <Marmota@192.168.1.104>
2016-04-09 00:18:33 +0900

    -Funcionando -Falta limpar o código -Colocar botoes de prints

--- BitCoinSystem/src/bitcoinsystem/MulticastListener.java
+++ BitCoinSystem/src/bitcoinsystem/MulticastListener.java
@@ -13,10 +13,13 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.DatagramPacket;
+import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.ByteBuffer;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  *
@@ -28,13 +31,16 @@ public class MulticastListener extends Thread{
     BitCoinSystem mainClass;
     MulticastSocket multiSocket;
     MessagePacket inPacket, outPacket;
+    InetAddress group;
     
 	public MulticastListener (int port, BitCoinSystem bcs) {
             
 		try{
                      this.port = port;
                      mainClass = bcs;
+                     group = InetAddress.getByName("224.0.0.10");
 			multiSocket = new MulticastSocket(port);
+                     multiSocket.joinGroup(group);
 			this.start();
 		} catch(IOException e) {System.out.println("Listen socket:"+e.getMessage());}
 	}
@@ -44,40 +50,69 @@ public class MulticastListener extends Thread{
            try{
 			while(true) {
 				byte[] buffer = new byte[1000];
-                        
-                            DatagramPacket getPacket = new DatagramPacket(buffer, buffer.length);
-                            multiSocket.receive(getPacket);
-                            ByteBuffer wrapped = ByteBuffer.wrap(getPacket.getData());
-                            
-                            inPacket = mainClass.getInputStream(wrapped.array());
-                            
-                            if(inPacket.messageID == mainClass.HELLO){
+                                System.out.println("debug, initiate multicast listen");
+                                DatagramPacket getPacket = new DatagramPacket(buffer, buffer.length);
+                                multiSocket.receive(getPacket);
+
+                                System.out.println("got something");
+                                ByteBuffer wrapped = ByteBuffer.wrap(getPacket.getData());
+
+                                inPacket = mainClass.getInputStream(wrapped.array());
+                                System.out.println("got this:" + inPacket.messageID);
                                 
-                                helloHandler(inPacket);
-                                
-                            } else if(inPacket.messageID == mainClass.CONFIRMTRANSACTION){
-                                
-                                transactionHandler(inPacket);
-                                
-                            } else if(inPacket.messageID == mainClass.VALIDATE){
-                                
-                                validateHandler(inPacket);
-                                
-                            }
+                                if(inPacket.messageID == mainClass.HELLO){
+
+                                    helloHandler(inPacket);
+
+                                } else if(inPacket.messageID == mainClass.CONFIRMTRANSACTION){
+
+                                    confirmTransactionHandler(inPacket);
+
+                                } else if(inPacket.messageID == mainClass.VALIDATE){
+                                    
+                                    validateHandler(inPacket);
+
+                                }
                             
                         }
-		} catch(Exception e) {System.out.println("Listen socket:"+e.getMessage());}
+		} catch(IOException e) {System.out.println("Listen socket:"+e.getMessage());}
        }
        
        public void helloHandler(MessagePacket message){
            
-           outPacket = new MessagePacket(mainClass.WELCOME, mainClass.ledger);
-           mainClass.sendUnicast(message.portID, outPacket);
-           
+           if(mainClass.applicationStarted == true){
+               System.out.println("toaqui");
+                outPacket = new MessagePacket(mainClass.WELCOME, mainClass.ledger);
+                mainClass.sendUnicast(message.portID, outPacket);
+           } else {
+               System.out.println("tolah");
+               //mainClass.trueReceivedSignal(message);
+               mainClass.ledger.addUser(message);
+               mainClass.userCounter++;
+               
+               if(mainClass.userCounter >= 2){
+                   Iterator it = mainClass.ledger.userList.entrySet().iterator();
+                   outPacket = new MessagePacket(mainClass.WELCOME, mainClass.ledger);
+                        while (it.hasNext()) {
+                            Map.Entry pair = (Map.Entry)it.next();
+                            System.out.println(pair.getKey() + " = " + pair.getValue());
+                            try{
+                                if(mainClass.port != (int)pair.getKey()){
+                                System.out.println((int)pair.getKey());
+                                mainClass.sendUnicast((int)pair.getKey(), outPacket);
+                                    System.out.println("Unicast de boas vindas enviado!");
+                                }
+                            }catch(ClassCastException e){
+                                System.out.println(e);
+                            }
+                        }
+               }
+           }
        }
        
-       public void transactionHandler(MessagePacket message){
-       
+       public void confirmTransactionHandler(MessagePacket message){
+           
+           System.out.println("transIDID:" + message.transID);
            mainClass.ledger.addTransaction(message.transID, message);
            
        }
@@ -85,12 +120,30 @@ public class MulticastListener extends Thread{
        public void validateHandler(MessagePacket message){
        
            mainClass.ledger.transactionConfirmed(message.transID);
-           
+           /*
            if(message.trans.senderPort == mainClass.port){
-               mainClass.wallet =- message.trans.value;
+               mainClass.wallet -= message.trans.value;
                mainClass.gui.setBitcoinAmountLabel(mainClass.wallet);
            } else if(message.trans.receiverPort == mainClass.port){
-               mainClass.wallet =+ message.trans.value;
+               mainClass.wallet += message.trans.value;
+               mainClass.gui.setBitcoinAmountLabel(mainClass.wallet);
+           }
+           */
+           MessagePacket pack = mainClass.ledger.transactionList.get(message.transID);
+           System.out.println(mainClass.ledger.transactionList.get(message.transID).messageID);
+           Transaction aux = mainClass.ledger.transactionList.get(message.transID).trans;
+           System.out.println(aux.receiverPort);
+           System.out.println(aux.senderPort);
+           if(aux.receiverPort == mainClass.port){
+               mainClass.wallet += aux.value;
+               mainClass.gui.setBitcoinAmountLabel(mainClass.wallet);
+           } else if(aux.senderPort == mainClass.port){
+               mainClass.wallet -= aux.value; 
+               mainClass.wallet -= mainClass.EXTRAREWARD;
+               mainClass.gui.setBitcoinAmountLabel(mainClass.wallet);
+           } 
+           if(message.trans.receiverPort == mainClass.port){
+               mainClass.wallet += message.trans.value;
                mainClass.gui.setBitcoinAmountLabel(mainClass.wallet);
            }
            
