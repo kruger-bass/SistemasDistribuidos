6907c6403cad51ac616920831ac4315a2046870c
Marmota <Marmota@192.168.1.104>
2016-04-30 17:02:00 +0900

    -base

---
+++ Turismo_Cliente/src/turismo_cliente/ClienteServo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package turismo_cliente;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+/**
+ *
+ * @author Marmota
+ */
+public class ClienteServo extends UnicastRemoteObject implements InterfaceCliente{
+    
+    public ClienteServo(InterfaceServidor server) throws RemoteException{}
+}
--- Turismo_Cliente/src/turismo_cliente/Turismo_Cliente.java
+++ Turismo_Cliente/src/turismo_cliente/Turismo_Cliente.java
@@ -5,6 +5,11 @@
  */
 package turismo_cliente;
 
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 /**
  *
  * @author kruger
@@ -14,8 +19,21 @@ public class Turismo_Cliente {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args){
         // TODO code application logic here
+        try{
+            int port = 2345;
+            Registry referenciaServicoNomes = LocateRegistry.getRegistry("localhost", port);
+            
+            InterfaceServidor server = (InterfaceServidor)referenciaServicoNomes.lookup("server");
+            
+            ClienteServo interfaceCliente = new ClienteServo(server);
+            
+        } catch(RemoteException e){
+            System.exit(0);
+        } catch(NotBoundException e){
+            System.exit(0);
+        }
     }
     
 }
